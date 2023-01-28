6729a757353d333f1c7b97c0f24d0545f777cae3
hikmetguner <h.hikmetguner@gmail.com>
2023-01-28 21:49:07 +0300

    Added character and weapon stat enumerations

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -13,4 +13,28 @@ namespace WIAFN.Constants
     {
         public static string Attacked = "Attacked";
     }
+
+    public enum CharacterStats
+    {
+        maxHealth = 0,
+        maxStamina = 1,
+        staminaRegen = 2,
+        healthRegen = 3,
+        jumpHeight = 4,
+        multiJumps = 5,
+        defaultSpeed = 6,
+        defaultSprintSpeed = 7,
+        defaultDashSpeed = 8,
+        multiDashes = 9,
+        dashDuration = 10,
+        dashCooldown = 11,
+        dashCost = 12,
+        speedCoefficient = 13,
+    }
+
+    public enum WeaponStats
+    {
+        Damage = 0,
+        FireRate = 1,
+    }
 }
